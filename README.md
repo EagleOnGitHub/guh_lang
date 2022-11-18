2e27574975d0682c28568b69d971062403bb154d
EagleOnGitHub <imnuex@gmail.com>
2022-11-18 17:01:11 +0200

    added repl to language, cleaned up readme, dat is now a vector represented by a pointer, rust optimizations in configuration

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # guh????
+
 guh language, nothing to see here
-# commands
+
+## commands
+
 |Command | What it does|
 |--------|-------------|
-|+ - * / | acc <operator> dat except for + - which add/sub 1|
-|[ ] ; ' | dat <operator> acc except for [ ] which add/sub 1|
-|o p     | print ascii from acc/dat respectively|
-|k l     | print number from acc/dat respectively|
+|+ - * / | acc [operator] current value of dat except for + - which add/sub 1|
+|[ ] ; ' | current value of dat [operator] acc except for [ ] which add/sub 1|
+|o p     | print ascii from acc/current value of dat respectively|
+|k l     | print number from acc/current value of dat respectively|
 |space   | print a space|
 |e       | print a new line|
-|f b     | forward or backward respectively label|
-|g n     | go forward or backward if acc == dat|
-|h m     | go forward or backward if acc > dat|
-|j <     | go forward or backward if acc < dat|
+|f b     | forward or backward label|
+|g n     | go forward or backward if acc == current value of dat|
+|h m     | go forward or backward if acc > current value of dat|
+|j <     | go forward or backward if acc < current value of dat|
+|q w     | go forward or backward in pointer of dat|
--- src/main.rs
+++ src/main.rs
@@ -1,64 +1,131 @@
 // Copyright 2022 EagleOnGitHub
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     http://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
 use std::env;
 use std::fs;
-use std::io::Read;
+use std::io::Read; // i feel mental pain seeing "owo" in a crate
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut file = fs::File::open(&args[1]).expect("failed opening file");
-    let mut contents = String::new();
-    let mut acc = 0;
-    let mut dat = 0;
+fn handle_command(s: String) {
+    let mut acc: i128 = 0;
+    let mut dat: Vec<i128> = vec!(16384);
+    let mut dat_pointer = 0;
     let mut backwards_label = 0;
     let mut forwards_label = 0;
-    let mut c = 'a';
-    let mut u = 0;
+    let mut c;
+    let mut u;
     let mut i = 0;
-
-    file.read_to_string(&mut contents)
-        .expect("failed reading file to string");
-
-    while i < contents.len() {
-        u = contents.as_bytes()[i];
+    while i < s.len() {
+        u = s.as_bytes()[i];
         c = char::from_u32(u.into()).unwrap();
         match c {
             '+' => acc += 1,
             '-' => acc -= 1,
-            '*' => acc *= dat,
-            '/' => acc /= dat,
-            '[' => dat += 1,
-            ']' => dat -= 1,
-            ';' => dat *= acc,
-            '\'' => dat /= acc,
-            'o' => print!("{}", char::from_u32(acc).unwrap()),
-            'p' => print!("{}", char::from_u32(dat).unwrap()),
+            '*' => acc *= dat[dat_pointer],
+            '/' => acc /= dat[dat_pointer],
+            '[' => dat[dat_pointer] += 1,
+            ']' => dat[dat_pointer] -= 1,
+            ';' => dat[dat_pointer] *= acc,
+            '\'' => dat[dat_pointer] /= acc,
+            'o' => print!("{}", char::from_u32(acc as u32).unwrap()),
+            'p' => print!("{}", char::from_u32(dat[dat_pointer] as u32).unwrap()),
             'k' => print!("{}", acc),
-            'l' => print!("{}", dat),
+            'l' => print!("{}", dat[dat_pointer]),
             ' ' => print!(" "),
-            'e' => print!("\n"),
+            'e' => println!(),
             'f' => forwards_label = i,
             'b' => backwards_label = i,
-            'g' => if acc == dat { i = forwards_label },
-            'n' => if acc == dat { i = backwards_label },
-            'h' => if acc > dat { i = forwards_label },
-            'm' => if acc > dat { i = backwards_label },
-            'j' => if acc < dat { i = forwards_label },
-            '<' => if acc < dat { i = backwards_label },
+            'g' => {
+                if acc == dat[dat_pointer] {
+                    i = forwards_label
+                }
+            }
+            'n' => {
+                if acc == dat[dat_pointer] {
+                    i = backwards_label
+                }
+            }
+            'h' => {
+                if acc > dat[dat_pointer] {
+                    i = forwards_label
+                }
+            }
+            'm' => {
+                if acc > dat[dat_pointer] {
+                    i = backwards_label
+                }
+            }
+            'j' => {
+                if acc < dat[dat_pointer] {
+                    i = forwards_label
+                }
+            }
+            '<' => {
+                if acc < dat[dat_pointer] {
+                    i = backwards_label
+                }
+            },
+            'q' => dat_pointer -= 1,
+            'w' => dat_pointer += 1,
             _ => todo!(),
         }
         i += 1;
+    }
+}
+
+fn repl() {
+    let mut rl = Editor::<()>::new().expect("failed creating line reader");
+    if rl.load_history("history.txt").is_err() {
+        println!("no previous history");
+    }
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                rl.add_history_entry(line.as_str());
+                handle_command(line);
+                println!();
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C pressed");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("EOF encountered or CTRL-D pressed");
+                break;
+            }
+            Err(err) => {
+                println!("unknown error: {:?}", err);
+                break;
+            }
+        }
+    }
+    rl.save_history("history.txt").expect("failed to write history file");
+}
+
+fn main() {
+    color_backtrace::install();
+
+    let mut contents = String::new();
+    let args: Vec<String> = env::args().collect();
+    if args.len() > 1 {
+        let mut file = fs::File::open(&args[1]).expect("failed opening file");
+        file.read_to_string(&mut contents)
+            .expect("failed reading file to string");
+        handle_command(contents)
+    } else {
+        repl()
     }
 }
